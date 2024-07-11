e4c0d2690ba3fbfd292467e07ec6d62f11fdb8ad
LiZhenhai-MBP14 <lizhenhai@LiZhenhaideMacBookPro14.local>
2024-07-11 11:38:28 +0800

    init

---
+++ merge2config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 获取项目根目录
+project_root=$(git rev-parse --show-toplevel)
+# 定义源目录和目标目录
+source_dir="$project_root"
+target_dir="$HOME/.config/clash"
+
+# 遍历当前目录下的所有 YAML 文件
+for source_file in "$source_dir"/*.yaml; do
+    # 读取源文件内容
+    content=$(cat "$source_file")
+
+    # 遍历目标目录下的所有 YAML 文件
+    for target_file in "$target_dir"/*.yaml; do
+        # 插入内容到rules:下的第一行
+        sed -i '/^rules:/a\'"$content"'' "$target_file"
+    done
+done
+
+echo "合并完成！"
+
