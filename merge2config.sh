72e8d36e68682331f4d315d7f76c2df170a1fdac
LiZhenhai-MBP14 <lizhenhai@LiZhenhaideMacBookPro14.local>
2024-07-11 12:23:22 +0800

    update

--- merge2config.sh
+++ merge2config.sh
@@ -16,13 +16,11 @@ for source_file in "$source_dir"/*.yaml; do
 
     # 遍历目标目录下的所有 YAML 文件
     for target_file in "$target_dir"/*.yaml; do
-        # 插入内容到rules:下的第一行
+        # 插入内容到rules:下的第一行 
         #sed -i '/^rules:/a\'"$content" "$target_file"
         #sed -i "/^rules:/a\\${content}" "$target_file"
         #sed -i -e '/^rules:/a\'$'\n'"$content"$'\n' "$target_file"
-        sed -i '' -e "/^rules:/a\\
-$content
-" "$target_file"
+        sed -i '' -e "/^rules:/a\\$content" "$target_file"
     done
 done
 
