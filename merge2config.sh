d13bb2bae6371dca19d71421f82e908e1c51667c
LiZhenhai-MBP14 <lizhenhai@LiZhenhaideMacBookPro14.local>
2024-07-11 12:18:25 +0800

    update

--- merge2config.sh
+++ merge2config.sh
@@ -13,7 +13,7 @@ for source_file in "$source_dir"/*.yaml; do
 
     # 添加换行符到内容的前后
     #content="\n${content}\n"
-    content=$(printf "\n%s\n" "$content")
+    #content=$(printf "\n%s\n" "$content")
 
     # 遍历目标目录下的所有 YAML 文件
     for target_file in "$target_dir"/*.yaml; do
